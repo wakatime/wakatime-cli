4535682571b4e286930bc7c9f96d0eb1a07671df
Carlos Henrique Guardao Gandarez <gandarez@gmail.com>
2020-03-29 16:23:59 -0400

    add nested levels to arguments

--- cmd/legacy/obsolete_arguments.go
+++ cmd/legacy/obsolete_arguments.go
@@ -1,9 +1,15 @@
 package legacy
 
+// ObsoleteArguments ObsoleteArguments
 type ObsoleteArguments struct {
 	File           string //file
 	HideFilenames1 bool   //hide-filenames
 	HideFilenames2 bool   //hidefilenames
 	LogFile        string //log-file
-	ApiUrl         string //apiurl
+	APIURL         string //apiurl
+}
+
+// NewObsoleteArgs NewObsoleteArgs
+func NewObsoleteArgs() *ObsoleteArguments {
+	return &ObsoleteArguments{}
 }
