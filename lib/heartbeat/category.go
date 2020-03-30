6cda9361b23a6f3cf92c42988e9afcc0acf9d4fc
dron22 <andy@fastback.io>
2020-03-30 22:45:42 +0200

    Add heartbeat category type
    Implements json.Marshaler and json.Unmarshaler interfaces to prepare
    storage in offline queue DB

---
+++ lib/heartbeat/category.go
@@ -0,0 +1,109 @@
+package heartbeat
+
+import (
+	"fmt"
+)
+
+// Category represents a heartbeat category
+type Category int
+
+const (
+	// UnknownCategory means category of user activity is unknown
+	UnknownCategory Category = iota
+	// BrowsingCategory means user is currently browsing
+	BrowsingCategory
+	// BuildingCategory means user is currently building
+	BuildingCategory
+	// CodeReviewingCategory means user is currently reviewing code
+	CodeReviewingCategory
+	// CodingCategory means user is currently coding
+	CodingCategory
+	// DebuggingCategory means user is currently debugging
+	DebuggingCategory
+	// DesigningCategory means user is currently designing
+	DesigningCategory
+	// IndexingCategory means user is currently indexing
+	IndexingCategory
+	// ManualTestingCategory means user is currently manual testing
+	ManualTestingCategory
+	// RunningTestsCategory means user is currently running tests
+	RunningTestsCategory
+	// WritingTestsCategory means user is currently writing tests
+	WritingTestsCategory
+)
+
+const (
+	unknownCategoryString       = "unknown"
+	browsingCategoryString      = "browsing"
+	buildingCategoryString      = "building"
+	codeReviewingCategoryString = "codeReviewing"
+	codingCategoryString        = "coding"
+	debuggingCategoryString     = "debugging"
+	designingCategoryString     = "designing"
+	indexingCategoryString      = "indexing"
+	manualTestingCategoryString = "manualTesting"
+	runningTestsCategoryString  = "runningTests"
+	writingTestsCategoryString  = "writingTests"
+)
+
+// UnmarshalJSON is a method to implement json.Unmarshaler interface
+func (c *Category) UnmarshalJSON(v []byte) error {
+	switch string(v) {
+	case unknownCategoryString:
+		*c = UnknownCategory
+	case browsingCategoryString:
+		*c = BrowsingCategory
+	case buildingCategoryString:
+		*c = BuildingCategory
+	case codeReviewingCategoryString:
+		*c = CodeReviewingCategory
+	case codingCategoryString:
+		*c = CodingCategory
+	case debuggingCategoryString:
+		*c = DebuggingCategory
+	case designingCategoryString:
+		*c = DesigningCategory
+	case indexingCategoryString:
+		*c = IndexingCategory
+	case manualTestingCategoryString:
+		*c = ManualTestingCategory
+	case runningTestsCategoryString:
+		*c = RunningTestsCategory
+	case writingTestsCategoryString:
+		*c = WritingTestsCategory
+	default:
+		return fmt.Errorf("unsupported category %q", v)
+	}
+
+	return nil
+}
+
+// MarshalJSON is a method to implement json.Marshaler interface
+func (c Category) MarshalJSON() ([]byte, error) {
+	switch c {
+	case UnknownCategory:
+		return []byte(unknownCategoryString), nil
+	case BrowsingCategory:
+		return []byte(browsingCategoryString), nil
+	case BuildingCategory:
+		return []byte(buildingCategoryString), nil
+	case CodeReviewingCategory:
+		return []byte(codeReviewingCategoryString), nil
+	case CodingCategory:
+		return []byte(codingCategoryString), nil
+	case DebuggingCategory:
+		return []byte(debuggingCategoryString), nil
+	case DesigningCategory:
+		return []byte(designingCategoryString), nil
+	case IndexingCategory:
+		return []byte(indexingCategoryString), nil
+	case ManualTestingCategory:
+		return []byte(manualTestingCategoryString), nil
+	case RunningTestsCategory:
+		return []byte(runningTestsCategoryString), nil
+	case WritingTestsCategory:
+		return []byte(writingTestsCategoryString), nil
+	default:
+		return nil, fmt.Errorf("unsupported category %v", c)
+	}
+}
---
+++ lib/heartbeat/category_test.go
@@ -0,0 +1,53 @@
+package heartbeat_test
+
+import (
+	"testing"
+
+	"github.com/alanhamlett/wakatime-cli/lib/heartbeat"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var categoryTests = map[string]heartbeat.Category{
+	"unknown":       heartbeat.UnknownCategory,
+	"browsing":      heartbeat.BrowsingCategory,
+	"building":      heartbeat.BuildingCategory,
+	"codeReviewing": heartbeat.CodeReviewingCategory,
+	"coding":        heartbeat.CodingCategory,
+	"debugging":     heartbeat.DebuggingCategory,
+	"designing":     heartbeat.DesigningCategory,
+	"indexing":      heartbeat.IndexingCategory,
+	"manualTesting": heartbeat.ManualTestingCategory,
+	"runningTests":  heartbeat.RunningTestsCategory,
+	"writingTests":  heartbeat.WritingTestsCategory,
+}
+
+func TestCategory_UnmarshalJSON(t *testing.T) {
+	for value, category := range categoryTests {
+		t.Run(value, func(t *testing.T) {
+			var c heartbeat.Category
+			err := c.UnmarshalJSON([]byte(value))
+			require.NoError(t, err)
+
+			assert.Equal(t, category, c)
+		})
+	}
+}
+
+func TestCategory_UnmarshalJSON_Invalid(t *testing.T) {
+	var c heartbeat.Category
+	err := c.UnmarshalJSON([]byte("invalid"))
+	require.Error(t, err)
+}
+
+func TestCategory_MarshalJSON(t *testing.T) {
+	for value, category := range categoryTests {
+		t.Run(value, func(t *testing.T) {
+			val, err := category.MarshalJSON()
+			require.NoError(t, err)
+
+			assert.Equal(t, value, string(val))
+		})
+	}
+}
