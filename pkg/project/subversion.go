5d57e735905f6b7535e3e0627223005fccb90643
Carlos Henrique Guardao Gandarez <gandarez@gmail.com>
2020-08-30 22:02:06 -0300

    add subversion project detection

---
+++ pkg/project/subversion.go
@@ -0,0 +1,146 @@
+package project
+
+import (
+	"fmt"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	jww "github.com/spf13/jwalterweatherman"
+	"github.com/yookoala/realpath"
+)
+
+// Subversion contains svn data.
+type Subversion struct {
+	// Filepath conaints the entity path.
+	Filepath string
+}
+
+// Detect gets information about the svn project for a given file.
+func (s Subversion) Detect() (Result, bool, error) {
+	binary, ok := findSvnBinary()
+	if !ok {
+		return Result{}, false,
+			Err("svn binary not found")
+	}
+
+	fp, err := realpath.Realpath(s.Filepath)
+	if err != nil {
+		return Result{}, false,
+			Err(fmt.Errorf("failed to get the real path: %w", err).Error())
+	}
+
+	// Take only the directory
+	if fileExists(fp) {
+		fp = path.Dir(fp)
+	}
+
+	// Find for .svn/wc.db file
+	svnConfigFile, ok := findSvnConfigFile(fp, ".svn", "wc.db")
+	if !ok {
+		return Result{}, false, nil
+	}
+
+	info, ok, err := svnInfo(path.Join(svnConfigFile, ".."), binary)
+	if err != nil {
+		return Result{}, false,
+			Err(fmt.Errorf("failed to get svn info: %w", err).Error())
+	}
+
+	if ok {
+		return Result{
+			Project: resolveSvnInfo(info, "Repository Root"),
+			Branch:  resolveSvnInfo(info, "URL"),
+		}, true, nil
+	}
+
+	return Result{}, false, nil
+}
+
+func findSvnConfigFile(fp string, directory string, match string) (string, bool) {
+	if fileExists(path.Join(fp, directory, match)) {
+		return path.Join(fp, directory), true
+	}
+
+	dir := filepath.Clean(path.Join(fp, ".."))
+	if dir == "/" {
+		return "", false
+	}
+
+	return findSvnConfigFile(dir, directory, match)
+}
+
+func svnInfo(fp string, binary string) (map[string]string, bool, error) {
+	if runtime.GOOS == "darwin" && !checkXcodeTools() {
+		return nil, false, nil
+	}
+
+	cmd := exec.Command(binary, "info", fp)
+	out, err := cmd.Output()
+
+	if err != nil {
+		return nil, false,
+			Err(fmt.Sprintf("error getting svn info: %s", err))
+	}
+
+	result := map[string]string{}
+
+	for _, line := range strings.Split(string(out), "\n") {
+		item := strings.Split(line, ": ")
+		if len(item) == 2 {
+			result[item[0]] = item[1]
+		}
+	}
+
+	return result, true, nil
+}
+
+func findSvnBinary() (string, bool) {
+	locations := []string{
+		"svn",
+		"/usr/bin/svn",
+		"/usr/local/bin/svn",
+	}
+
+	for _, loc := range locations {
+		cmd := exec.Command(loc, "--version")
+
+		err := cmd.Run()
+		if err != nil {
+			jww.ERROR.Printf("failed while calling %s --version: %s", loc, err)
+			continue
+		}
+
+		return loc, true
+	}
+
+	return "", false
+}
+
+func checkXcodeTools() bool {
+	cmd := exec.Command("/usr/bin/xcode-select", "-p")
+
+	err := cmd.Run()
+
+	return err == nil
+}
+
+func resolveSvnInfo(info map[string]string, key string) string {
+	if val, ok := info[key]; ok {
+		parts := strings.Split(val, "/")
+		last := parts[len(parts)-1]
+		parts2 := strings.Split(last, "\\")
+		last2 := parts2[len(parts2)-1]
+
+		return last2
+	}
+
+	return ""
+}
+
+// String returns its name.
+func (s Subversion) String() string {
+	return "svn-detector"
+}
---
+++ pkg/project/subversion_test.go
@@ -0,0 +1,127 @@
+package project_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"testing"
+
+	"github.com/wakatime/wakatime-cli/pkg/project"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSubversion_Detect(t *testing.T) {
+	fp, tearDown := setupTestSvn(t)
+	defer tearDown()
+
+	s := project.Subversion{
+		Filepath: path.Join(fp, "wakatime-cli/src/pkg/file.go"),
+	}
+
+	result, detected, err := s.Detect()
+	require.NoError(t, err)
+
+	assert.True(t, detected)
+	assert.Equal(t, project.Result{
+		Project: "wakatime-cli",
+		Branch:  "trunk",
+	}, result)
+}
+
+func TestSubversion_Detect_Branch(t *testing.T) {
+	fp, tearDown := setupTestSvnBranch(t)
+	defer tearDown()
+
+	s := project.Subversion{
+		Filepath: path.Join(fp, "wakatime-cli/src/pkg/file.go"),
+	}
+
+	result, detected, err := s.Detect()
+	require.NoError(t, err)
+
+	assert.True(t, detected)
+	assert.Equal(t, project.Result{
+		Project: "wakatime-cli",
+		Branch:  "billing",
+	}, result)
+}
+
+func setupTestSvn(t *testing.T) (fp string, tearDown func()) {
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "wakatime-svn")
+	require.NoError(t, err)
+
+	err = os.MkdirAll(path.Join(tmpDir, "wakatime-cli/src/pkg"), os.FileMode(int(0700)))
+	require.NoError(t, err)
+
+	tmpFile, err := os.Create(path.Join(tmpDir, "wakatime-cli/src/pkg/file.go"))
+	require.NoError(t, err)
+
+	tmpFile.Close()
+
+	copyDir(t, "testdata/svn", path.Join(tmpDir, "wakatime-cli/.svn"))
+
+	return tmpDir, func() { os.RemoveAll(tmpDir) }
+}
+
+func setupTestSvnBranch(t *testing.T) (fp string, tearDown func()) {
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "wakatime-svn")
+	require.NoError(t, err)
+
+	err = os.MkdirAll(path.Join(tmpDir, "wakatime-cli/src/pkg"), os.FileMode(int(0700)))
+	require.NoError(t, err)
+
+	tmpFile, err := os.Create(path.Join(tmpDir, "wakatime-cli/src/pkg/file.go"))
+	require.NoError(t, err)
+
+	tmpFile.Close()
+
+	copyDir(t, "testdata/svn_branch", path.Join(tmpDir, "wakatime-cli/.svn"))
+
+	return tmpDir, func() { os.RemoveAll(tmpDir) }
+}
+
+func copyDir(t *testing.T, src string, dst string) {
+	src = filepath.Clean(src)
+	dst = filepath.Clean(dst)
+
+	si, err := os.Stat(src)
+	require.NoError(t, err)
+
+	if !si.IsDir() {
+		return
+	}
+
+	_, err = os.Stat(dst)
+	if err != nil && !os.IsNotExist(err) {
+		return
+	}
+
+	if err == nil {
+		return
+	}
+
+	err = os.MkdirAll(dst, si.Mode())
+	require.NoError(t, err)
+
+	entries, err := ioutil.ReadDir(src)
+	require.NoError(t, err)
+
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+
+		if entry.IsDir() {
+			copyDir(t, srcPath, dstPath)
+		} else {
+			// Skip symlinks.
+			if entry.Mode()&os.ModeSymlink != 0 {
+				continue
+			}
+
+			copyFile(t, srcPath, dstPath)
+		}
+	}
+}
