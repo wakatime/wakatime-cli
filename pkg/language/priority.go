35e15eab230c6fa8277301859c3bafd459a1e794
andy <andy@fastback.io>
2020-11-29 07:24:14 +0100

    Adjust typescript priority to match pygments value
    While pygments preferes TypeScript over TypoScript, chrome
    works the other way around, as typoscript has an assigned
    priority of `0.5.`. This PR fixes this to achieve the same
    behaviour like pygments.

--- pkg/language/priority.go
+++ pkg/language/priority.go
@@ -2,9 +2,11 @@ package language
 
 func priority(lang string) (float32, bool) {
 	prios := map[string]float32{
-		"FSharp":     0.01,
-		"Perl":       0.01,
-		"TypeScript": 0.01,
+		"FSharp": 0.01,
+		"Perl":   0.01,
+		// Higher priority than the TypoScriptLexer, as TypeScript is far more
+		// common these days
+		"TypeScript": 0.5,
 	}
 
 	p, ok := prios[lang]
