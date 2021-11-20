a3f984569125bcfdd78a79318e00b51c22415e3a
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2021-11-20 13:55:22 -0300

    Fix windows linter errors

--- pkg/api/transport_windows.go
+++ pkg/api/transport_windows.go
@@ -9,26 +9,38 @@ import (
 )
 
 func loadSystemRoots() (*x509.CertPool, error) {
-	const CRYPT_E_NOT_FOUND = 0x80092004
+	const CryptENotFound = 0x80092004
 
-	store, err := syscall.CertOpenSystemStore(0, syscall.StringToUTF16Ptr("ROOT"))
+	rootPtr, err := syscall.UTF16PtrFromString("ROOT")
 	if err != nil {
 		return nil, err
 	}
-	defer syscall.CertCloseStore(store, 0)
+
+	store, err := syscall.CertOpenSystemStore(0, rootPtr)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		_ = syscall.CertCloseStore(store, 0)
+	}()
 
 	roots := x509.NewCertPool()
+
 	var cert *syscall.CertContext
+
 	for {
 		cert, err = syscall.CertEnumCertificatesInStore(store, cert)
 		if err != nil {
 			if errno, ok := err.(syscall.Errno); ok {
-				if errno == CRYPT_E_NOT_FOUND {
+				if errno == CryptENotFound {
 					break
 				}
 			}
+
 			return nil, err
 		}
+
 		if cert == nil {
 			break
 		}
@@ -36,9 +48,11 @@ func loadSystemRoots() (*x509.CertPool, error) {
 		buf := (*[1 << 20]byte)(unsafe.Pointer(cert.EncodedCert))[:cert.Length:cert.Length]
 		buf2 := make([]byte, cert.Length)
 		copy(buf2, buf)
+
 		if c, err := x509.ParseCertificate(buf2); err == nil {
 			roots.AddCert(c)
 		}
 	}
+
 	return roots, nil
 }
