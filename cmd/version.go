ba4fed3e696448c41586b4a2b801b7e753309396
Harsh Singh <63696299+harshsingh32@users.noreply.github.com>
2023-04-23 16:58:12 +0530

    Update version.go

--- cmd/version.go
+++ cmd/version.go
@@ -8,8 +8,9 @@ import (
 
 	"github.com/spf13/viper"
 )
+// runVersion displays the version of the wakatime-cli tool.
 
-func runVersion(v *viper.Viper) (int, error) {
+func runVersion(v *viper.Viper) (exitCode int, err error) {
 	if v.GetBool("verbose") {
 		fmt.Printf(
 			"wakatime-cli\n  Version: %s\n  Commit: %s\n  Built: %s\n  OS/Arch: %s/%s\n",
@@ -20,10 +21,11 @@ func runVersion(v *viper.Viper) (int, error) {
 			version.Arch,
 		)
 
-		return exitcode.Success, nil
+		exitCode = exitcode.Success
+	} else {
+		fmt.Println(version.Version)
+                exitCode = exitcode.Success
 	}
 
-	fmt.Println(version.Version)
-
-	return exitcode.Success, nil
+	return exitCode, nil
 }
