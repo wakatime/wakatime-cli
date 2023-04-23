7e1fee49e2ff74da26d20815c14f23fd595073a4
Harsh Singh <63696299+harshsingh32@users.noreply.github.com>
2023-04-23 17:32:37 +0530

    Update main.go

--- testdata/main.go
+++ testdata/main.go
@@ -4,8 +4,16 @@ import (
 	"fmt"
 	"os"
 )
+const exitSuccess = 0
 
 func main() {
+	// Print a greeting message
 	fmt.Println("hello world")
-	os.Exit(0)
+
+	// Exit with a success status code
+	if err := os.Exit(exitSuccess); 
+        err != nil {
+		panic(err)
+	}
 }
+	
