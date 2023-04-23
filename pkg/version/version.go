26eef15e8af60f7eb0e85f8ea7be81e83bb5d5ec
Harsh Singh <63696299+harshsingh32@users.noreply.github.com>
2023-04-23 09:58:36 +0530

    Update version.go

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,13 +3,13 @@ package version
 // nolint:gochecknoglobals
 var (
 	// Arch specifies the CPU architecture of the wakatime-cli build.
-	Arch = "unset"
+	Arch = "unknown"
 	// BuildDate states the date of the wakatime-cli build.
-	BuildDate = "unset"
+	BuildDate = "unknown"
 	// Commit states the commit of the wakatime-cli build.
-	Commit = "unset"
+	Commit = "unknown"
 	// OS specifies the target operating system of the wakatime-cli build.
-	OS = "unset"
+	OS = "unknown"
 	// Version states the version of the wakatime-cli build.
-	Version = "unset"
+	Version = "unknown"
 )
