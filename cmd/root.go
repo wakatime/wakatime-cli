79242dc522e19f69146274ccb54a1237f1c4751f
Alan Hamlett <alan.hamlett@gmail.com>
2025-07-13 21:11:04 -0500

    Add AI Coding category

--- cmd/root.go
+++ cmd/root.go
@@ -75,7 +75,7 @@ func setFlags(cmd *cobra.Command, v *viper.Viper) {
 	flags.String(
 		"category",
 		"",
-		"Category of this heartbeat activity. Can be \"coding\","+
+		"Category of this heartbeat activity. Can be \"coding\", \"ai coding\", "+
 			" \"building\", \"indexing\", \"debugging\", \"learning\","+
 			" \"meeting\", \"planning\", \"researching\", \"communicating\", \"supporting\" "+
 			" \"advising\", \"running tests\", \"writing tests\", \"manual testing\","+
