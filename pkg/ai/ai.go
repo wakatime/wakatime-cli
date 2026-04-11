5ff003f482a2f74360a5ee6c1df209052c931a1a
Alan Hamlett <alan.hamlett@gmail.com>
2026-04-11 19:04:27 -0400

    Only change to AI category when before first human edit

--- pkg/ai/ai.go
+++ pkg/ai/ai.go
@@ -141,7 +141,7 @@ func WithAISync(config Config) heartbeat.HandleOption {
 				return next(ctx, hh)
 			}
 
-			heartbeats = preserveAttributes(heartbeats, hh)
+			heartbeats, firstHumanEdit := preserveAttributes(heartbeats, hh)
 
 			heartbeats = applyProject(heartbeats, config)
 
@@ -188,7 +188,7 @@ func WithAISync(config Config) heartbeat.HandleOption {
 					h.Category = "ai coding"
 				}
 
-				if (h.HumanLineChanges == nil || *h.HumanLineChanges == 0) && inRange(30) {
+				if (firstHumanEdit == nil || *firstHumanEdit > h.Time) && inRange(30) {
 					h.Category = "ai coding"
 				}
 
@@ -314,15 +314,21 @@ func getLastParsedAt(ctx context.Context, v *viper.Viper) (time.Time, error) {
 // preserveAttributes mutates aiHeartbeats pulling in the attributes from
 // humanHeartbeats, which should normally have more details already populated
 // from the IDE than available on aiHeartbeats.
-func preserveAttributes(aiHeartbeats []heartbeat.Heartbeat, humanHeartbeats []heartbeat.Heartbeat) Heartbeats {
+func preserveAttributes(aiHeartbeats []heartbeat.Heartbeat, humanHeartbeats []heartbeat.Heartbeat) (Heartbeats, *float64) {
 	if len(humanHeartbeats) == 0 {
-		return aiHeartbeats
+		return aiHeartbeats, nil
 	}
 
 	originals := make(map[string][]heartbeat.Heartbeat, len(humanHeartbeats))
 	fallbackProjectFolder := ""
 
+	var firstHumanEdit *float64
+
 	for _, h := range humanHeartbeats {
+		if (firstHumanEdit == nil || h.Time < *firstHumanEdit) && h.HumanLineChanges != nil && *h.HumanLineChanges != 0 {
+			firstHumanEdit = &h.Time
+		}
+
 		originals[h.Entity] = append(originals[h.Entity], h)
 
 		if fallbackProjectFolder == "" {
@@ -345,7 +351,7 @@ func preserveAttributes(aiHeartbeats []heartbeat.Heartbeat, humanHeartbeats []he
 		}
 	}
 
-	return aiHeartbeats
+	return aiHeartbeats, firstHumanEdit
 }
 
 func preserveAttributesFromHumanHeartbeat(aiHeartbeat *heartbeat.Heartbeat, human heartbeat.Heartbeat) {
